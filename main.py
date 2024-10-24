af2ca3eb704450bc56cbcc8355246b14e7be855b
Aleksander <sanya.slepow2015@yandex.ru>
2024-10-24 19:54:19 +0500

    commit3

--- main.py
+++ main.py
@@ -1,38 +1,50 @@
+# Импорт встроенной библиотеки для работы веб-сервера
 from http.server import BaseHTTPRequestHandler, HTTPServer
-import os
+from src.config import file_contact_html
 
-# Настройки запуска
-HOST_NAME = "localhost"
-SERVER_PORT = 8080
+# Для начала определим настройки запуска
+hostName = "localhost"  # Адрес для доступа по сети
+serverPort = 8080  # Порт для доступа по сети
+
 
 class MyServer(BaseHTTPRequestHandler):
+    """
+    Специальный класс, который отвечает за
+    обработку входящих запросов от клиентов
+    """
     def do_GET(self):
-        # Проверяем, какой URL был запрошен
-        if self.path == "/":
-            # Если запрошен корневой URL, отправляем страницу "Контакты"
-            self.send_response(200)
-            self.send_header("Content-type", "text/html")
-            self.end_headers()
+        """ Метод для обработки входящих GET-запросов
+        """
+        self.send_response(200)  # Отправка кода ответа
+        self.send_header("Content-type", "text/html")  # Отправка типа данных, который будет передаваться
+        self.end_headers()
+        with open(file_contact_html, encoding='utf-8') as f:
+            content = f.read()
+        # Завершение формирования заголовков ответа
+        self.wfile.write(bytes(content, "utf-8"))  # Тело ответа
 
-            # Читаем содержимое HTML-файла с контактами
-            with open("contact.html", "r") as f:
-                html_content = f.read()
-            self.wfile.write(bytes(html_content, "utf-8"))
-        else:
-            # Если запрошен другой URL, отправляем 404 Not Found
-            self.send_response(404)
-            self.send_header("Content-type", "text/html")
-            self.end_headers()
-            self.wfile.write(bytes("<h1>404 Not Found</h1>", "utf-8"))
+    def do_POST(self):
+        content_length = int(self.headers["Content-Length"])
+        post_data = self.rfile.read(content_length)
+        print(post_data)
+
+        response = f"Received POST data: {post_data.decode('utf-8')}"
+        print(response)
+
 
 if __name__ == "__main__":
-    webServer = HTTPServer((HOST_NAME, SERVER_PORT), MyServer)
-    print(f"Server started http://{HOST_NAME}:{SERVER_PORT}")
+    # Инициализация веб-сервера, который будет по заданным параметрах в сети
+    # принимать запросы и отправлять их на обработку специальному классу, который был описан выше
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
 
     try:
+        # Cтарт веб-сервера в бесконечном цикле прослушивания входящих запросов
         webServer.serve_forever()
     except KeyboardInterrupt:
+        # Корректный способ остановить сервер в консоли через сочетание клавиш Ctrl + C
         pass
 
+    # Корректная остановка веб-сервера, чтобы он освободил адрес и порт в сети, которые занимал
     webServer.server_close()
     print("Server stopped.")
